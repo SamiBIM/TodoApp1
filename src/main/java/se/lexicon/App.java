9b64beb74db3dc089f98e8299eca54232f965cb6
Sami Burhan <samiburhaan@gmail.com>
2024-07-01 19:55:51 +0200

    Initial commit for App class

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,34 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
+import java.time.LocalDate;
+
 public class App 
 {
+
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Person p1 = new Person();
+        p1.setFirstName("Sami");
+        p1.setLastName("Burhan");
+        p1.setEmail("s.b@gmail");
+        TodoItem todoItem = new TodoItem();
+        todoItem.setTitle("Cook");
+        todoItem.setDeadLine(LocalDate.now().plusDays(1));
+        todoItem.setCreator(p1);
+        System.out.println(todoItem.getSummary());
+        System.out.println(todoItem.isOverdue());
+
+        TodoItemTask todoItemTask = new TodoItemTask();
+        todoItemTask.setAssignee(p1);
+        todoItemTask.setAssigned(false);
+        System.out.println(todoItemTask.getSummary());
+        System.out.println(todoItemTask.isAssigned());
+
+
+
     }
 }
+
+
